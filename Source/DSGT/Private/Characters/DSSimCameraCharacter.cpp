93ced7309671b6a6e7b59e4130ad4bfc8104460a
Renan Souza <renan.souza@powerpush.com.br>
2025-03-02 02:50:11 -0300

    on development

---
+++ Source/DSGT/Private/Characters/DSSimCameraCharacter.cpp
@@ -0,0 +1,16 @@
+﻿#include "Characters/DSSimCameraCharacter.h"
+
+// ===========================================================================================
+// Life Cycle Methods:
+// ===========================================================================================
+ADSSimCameraCharacter::ADSSimCameraCharacter() {
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ADSSimCameraCharacter::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void ADSSimCameraCharacter::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
