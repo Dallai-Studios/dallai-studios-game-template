48eccd35d93c50960df01db8b49df8596e3c4ca7
Renan Souza <renan.souza@powerpush.com.br>
2025-03-25 16:54:29 -0300

    new enum for game client state and the state control is done now

---
+++ Source/DSGT/Public/Enums/EDSBaseGameClientHudState.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "EDSBaseGameClientHudState.generated.h"
+
+UENUM(BlueprintType, Blueprintable)
+enum class EdsBaseGameClientHudState : uint8 {
+	GAME_SETTINGS,
+	AUDIO_SETTINGS,
+	VIDEO_SETTINGS
+};
