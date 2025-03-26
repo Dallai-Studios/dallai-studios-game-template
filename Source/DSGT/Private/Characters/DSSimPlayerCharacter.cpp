9dc45b881e74b723b4bfceb9a6c793a222432c2b
Renan Souza <renan.souza@powerpush.com.br>
2025-03-26 07:40:17 -0300

    on development

---
+++ Source/DSGT/Private/Characters/DSSimPlayerCharacter.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2025 Dallai Studios. All Rights Reserved.
+
+#include "Characters/DSSimPlayerCharacter.h"
+
+ADSSimPlayerCharacter::ADSSimPlayerCharacter() {
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ADSSimPlayerCharacter::BeginPlay() {
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ADSSimPlayerCharacter::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
---
+++ Source/DSGT/Public/Characters/DSSimPlayerCharacter.h
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2025 Dallai Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "DSSimPlayerCharacter.generated.h"
+
+UCLASS()
+class DSGT_API ADSSimPlayerCharacter : public ACharacter {
+	GENERATED_BODY()
+
+public:
+	ADSSimPlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
