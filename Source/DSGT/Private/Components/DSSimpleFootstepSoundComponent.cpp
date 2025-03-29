83d6d892a02a397f19a3379e8d6333ad573bcae7
Renan Souza <renan.souza@powerpush.com.br>
2025-03-28 23:34:46 -0300

    new Simple footstep sound player component

---
+++ Source/DSGT/Private/Components/DSSimpleFootstepSoundComponent.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2025 Dallai Studios. All Rights Reserved.
+
+#include "Components/DSSimpleFootstepSoundComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// ==============================================================
+// Lifecycle Methods:
+// ==============================================================
+UDSSimpleFootstepSoundComponent::UDSSimpleFootstepSoundComponent() {
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UDSSimpleFootstepSoundComponent::BeginPlay() {
+	Super::BeginPlay();
+}
+
+
+
+// ==============================================================
+// Component Methods:
+// ==============================================================
+void UDSSimpleFootstepSoundComponent::PlayWalkFootstepSound() {
+	
+}
+
+void UDSSimpleFootstepSoundComponent::PlayRunningFootstepSound() {
+	
+}
+
+void UDSSimpleFootstepSoundComponent::PlayStartJumpSound() {
+	
+}
+
+void UDSSimpleFootstepSoundComponent::PlayFallLandSound() {
+	
+}
---
+++ Source/DSGT/Public/Components/DSSimpleFootstepSoundComponent.h
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2025 Dallai Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DSSimpleFootstepSoundComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class DSGT_API UDSSimpleFootstepSoundComponent : public UActorComponent {
+	GENERATED_BODY()
+
+public:
+	// ==============================================================
+	// Sound To Play:
+	// ==============================================================
+	UPROPERTY(EditAnywhere, Category="Sounds To Play")
+	TObjectPtr<class USoundBase> walkSound;
+
+	UPROPERTY(EditAnywhere, Category="Sounds To Play")
+	TObjectPtr<USoundBase> runningSound;
+
+	UPROPERTY(EditAnywhere, Category="Sounds To Play")
+	TObjectPtr<USoundBase> jumpStartSound;
+
+	UPROPERTY(EditAnywhere, Category="Sounds To Play")
+	TObjectPtr<USoundBase> fallLandSound;
+	
+
+	
+public:
+	// ==============================================================
+	// Lifecycle Methods:
+	// ==============================================================
+	UDSSimpleFootstepSoundComponent();
+	virtual void BeginPlay() override;
+
+
+
+	// ==============================================================
+	// Component Methods:
+	// ==============================================================
+	UFUNCTION(BlueprintCallable, Category="Component Methods")
+	void PlayWalkFootstepSound();
+
+	UFUNCTION(BlueprintCallable, Category="Component Methods")
+	void PlayRunningFootstepSound();
+
+	UFUNCTION(BlueprintCallable, Category="Component Methods")
+	void PlayStartJumpSound();
+
+	UFUNCTION(BlueprintCallable, Category="Component Methods")
+	void PlayFallLandSound();
+};
