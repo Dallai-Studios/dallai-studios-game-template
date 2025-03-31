0b6dbb59f1ce653195526e859b79610c4fb9bd5a
Renan Souza <renan.souza@powerpush.com.br>
2025-03-31 02:09:54 -0300

    on development

---
+++ Source/DSGT/Private/Components/DSSimpleFootstepSoundPlayer.cpp
@@ -0,0 +1,72 @@
+﻿// Copyright (c) 2025 Dallai Studios. All Rights Reserved.
+
+#include "Components/DSSimpleFootstepSoundPlayer.h"
+#include "Kismet/GameplayStatics.h"
+
+UDSSimpleFootstepSoundPlayer::UDSSimpleFootstepSoundPlayer() {
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UDSSimpleFootstepSoundPlayer::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void UDSSimpleFootstepSoundPlayer::PlayWalkFootstepSound() {
+	if (this->runningSoundTimeHandler.IsValid()) this->GetWorld()->GetTimerManager().ClearTimer(this->runningSoundTimeHandler);
+	
+	this->GetWorld()->GetTimerManager().SetTimer(
+		this->walkSoundTimeHandler,
+		this,
+		&UDSSimpleFootstepSoundPlayer::DetectAndPlayWalkSoundBasedOnSurface,
+		this->walkTimerRate,
+		true
+	);
+}
+
+void UDSSimpleFootstepSoundPlayer::PlayRunningFootstepSound() {
+	if (this->walkSoundTimeHandler.IsValid()) this->GetWorld()->GetTimerManager().ClearTimer(this->walkSoundTimeHandler);
+	
+	this->GetWorld()->GetTimerManager().SetTimer(
+		this->runningSoundTimeHandler,
+		this,
+		&UDSSimpleFootstepSoundPlayer::DetectAndPlayRunningSoundBasedOnSurface,
+		this->runningTimerRate,
+		true
+	);
+}
+
+void UDSSimpleFootstepSoundPlayer::DetectAndPlayWalkSoundBasedOnSurface() {
+	FHitResult hitResult;
+	auto startLocation = this->GetOwner()->GetActorLocation();
+	auto endLocation = startLocation + ((this->GetOwner()->GetActorUpVector() * -1) * this->floorDetectionLineSize);
+	FCollisionQueryParams params;
+	FCollisionResponseParams responseParams;
+
+	bool hit = this->GetWorld()->LineTraceSingleByChannel(hitResult, startLocation, endLocation, ECC_Visibility, params, responseParams);
+
+	if (hit) {
+		UPhysicalMaterial* physMaterial = hitResult.PhysMaterial.Get();
+		if (physMaterial) {
+			EPhysicalSurface surfaceType = physMaterial->SurfaceType;
+			switch (surfaceType) {
+			case SurfaceType1: // Concrete
+				UGameplayStatics::PlaySound2D(this->GetWorld(), this->concreteFootstepSound);
+				break;
+			case SurfaceType2: // Wood
+				UGameplayStatics::PlaySound2D(this->GetWorld(), this->woodFootstepSound);
+				break;
+			case SurfaceType3: // Grass
+				UGameplayStatics::PlaySound2D(this->GetWorld(), this->grassFootstepSound);
+				break;
+			default:
+				UGameplayStatics::PlaySound2D(this->GetWorld(), this->concreteFootstepSound);
+				break;
+			}
+		}
+	}
+}
+
+void UDSSimpleFootstepSoundPlayer::DetectAndPlayRunningSoundBasedOnSurface() {
+	
+}
+
---
+++ Source/DSGT/Public/Components/DSSimpleFootstepSoundPlayer.h
@@ -0,0 +1,61 @@
+﻿// Copyright (c) 2025 Dallai Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DSSimpleFootstepSoundPlayer.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class DSGT_API UDSSimpleFootstepSoundPlayer : public UActorComponent {
+	GENERATED_BODY()
+
+private:
+	// ==================================================
+	// Component Configuration
+	// ==================================================
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Component Configuration")
+	float floorDetectionLineSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Component Configuration")
+	TObjectPtr<class USoundBase> concreteFootstepSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Component Configuration")
+	TObjectPtr<USoundBase> woodFootstepSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Component Configuration")
+	TObjectPtr<USoundBase> grassFootstepSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Component Configuration")
+	float walkTimerRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Component Configuration")
+	float runningTimerRate;
+
+	// ==================================================
+	// Timer Handlers
+	// ==================================================
+	FTimerHandle walkSoundTimeHandler;
+	FTimerHandle runningSoundTimeHandler;
+	
+public:
+	// ==================================================
+	// Lifecycle Methods
+	// ==================================================
+	UDSSimpleFootstepSoundPlayer();
+	virtual void BeginPlay() override;
+	
+	// ==================================================
+	// Component Methods
+	// ==================================================
+	UFUNCTION(BlueprintCallable, Category="Component Methods")
+	void PlayWalkFootstepSound();
+
+	UFUNCTION(BlueprintCallable, Category="Component Methods")
+	void PlayRunningFootstepSound();
+
+private:
+	void DetectAndPlayWalkSoundBasedOnSurface();
+	void DetectAndPlayRunningSoundBasedOnSurface();
+};
