a679b8a26695eb6955f27fd4b84024a6407de3d2
Renan Souza <renan.souza@powerpush.com.br>
2025-04-06 03:52:24 -0300

    getting the interaction system better

--- Source/DSGT/Public/Enums/EDSCharacterSimpleState.h
+++ Source/DSGT/Public/Enums/EDSCharacterSimpleState.h
@@ -10,4 +10,5 @@ enum class EDSCharacterSimpleState : uint8 {
 	RUNNING,
 	FALLING,
 	DASHING,
+	INTERACTING
 };
---
+++ Source/DSGT/Public/Enums/EDSInteractionType.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "EDSInteractionType.generated.h"
+
+UENUM(BlueprintType, Blueprintable)
+enum class EDSInteractionType : uint8 {
+	PICKABLE,
+	STATIC,
+	NPC
+};
