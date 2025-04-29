0c053d8a1505d296d07160ea15750d2d2d9ec847
Renan Souza <renan.souza@powerpush.com.br>
2025-04-29 13:28:38 -0300

    on development

--- Source/DSGT/DSDialogueHUD.cpp
+++ Source/DSGT/DSDialogueHUD.cpp
@@ -1,13 +1,28 @@
 ﻿// Copyright (c) 2025 Dallai Studios. All Rights Reserved.
 
-
 #include "DSDialogueHUD.h"
-void UDSDialogueHUD::BuildDialogueData(FText characterName, FString dialogueLine, float dialogueDuration) {
-	this->cachedCharacterName = characterName;
-	this->cachedDialogueLine = dialogueLine;
-	this->cacehdDialogueDuration = dialogueDuration;
+
+void UDSDialogueHUD::StartNewDialogue(FText characterName, FString& dialogueText, float dialogueAnimationInterval) {
+	this->dialogueStringText = "";
+	this->dialogueFullStringText = dialogueFullStringText;
+	this->currentTextIndex = 0;
+
+	this->GetWorld()->GetTimerManager().ClearTimer(this->dialogueTypingHandle);
+
+	this->GetWorld()->GetTimerManager().SetTimer(
+		this->dialogueTypingHandle,
+		this,
+		&UDSDialogueHUD::UpdateDialogueText,
+		dialogueAnimationInterval,
+		true
+	);
 }
 
-void UDSDialogueHUD::AnimateDialogue() {
-	
+void UDSDialogueHUD::UpdateDialogueText() {
+	if (this->currentTextIndex < this->dialogueFullStringText.Len()) {
+		this->dialogueStringText += this->dialogueFullStringText[currentTextIndex++];
+		this->finalDialogueText = FText::FromString(this->dialogueStringText);
+	} else {
+		this->GetWorld()->GetTimerManager().ClearTimer(this->dialogueTypingHandle);
+	}
 }
--- Source/DSGT/DSDialogueHUD.h
+++ Source/DSGT/DSDialogueHUD.h
@@ -6,20 +6,35 @@
 #include "Blueprint/UserWidget.h"
 #include "DSDialogueHUD.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDialogueHudEvent);
+
 UCLASS()
 class DSGT_API UDSDialogueHUD : public UUserWidget {
 	GENERATED_BODY()
 
 public:
-	FTimerHandle dialogueTypingTimer;
-	FString fullText;
-	FString currentText;
-	int32 currentIndex = 0;
+	UPROPERTY(BlueprintReadOnly, Category="HUD Data")
+	FText characterName; // I can use this to create bind events to get the character name on the HUD itself
+
+	UPROPERTY(BlueprintReadOnly, Category="HUD Data")
+	FText finalDialogueText; // also for this value here
+
+	// this variable will hold the full string when the StartNewDialogue function is called. -Dallai
+	FString dialogueFullStringText;
+
+	// This variable is responsable for creating the typing effect. -Dallai
+	FString dialogueStringText;
+
+	// This only serve for the animation. -Dallai
+	int32 currentTextIndex = 0;
+	FTimerHandle dialogueTypingHandle;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FDialogueHudEvent OnFinishCurrentDialogue; // I dont know if I need this but, better safe then sorry. -Dallai
 	
 public:
 	UFUNCTION(BlueprintCallable)
-	void BuildDialogueData(FText characterName, FString dialogueLine, float dialogueDuration);
+	void StartNewDialogue(FText characterName, FString& dialogueText, float dialogueAnimationInterval);
 
-	UFUNCTION(BlueprintCallable)
-	void AnimateDialogue();
+	void UpdateDialogueText();
 };
